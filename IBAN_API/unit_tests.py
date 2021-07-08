c8446d6f26e51c09d515b7254bd58df4f44ec881
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-07-08 12:09:19 +0200

    IBAN_API

---
+++ IBAN_API/unit_tests.py
@@ -0,0 +1,24 @@
+import unittest
+import requests
+
+
+class UnitTestsIbanAPI(unittest.TestCase):
+    # https://ibanapi.com/get-api
+    def test_get_get_api(self):
+        print('test_get_get_api')
+
+        params = (
+            ('api_key', 'API_KEY'),
+        )
+
+        iban = "EE471000001020145685"
+
+        url = "https://api.ibanapi.com/v1/validate/" + iban
+
+        response = requests.get(url, params=params)
+
+        print(response.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
